0de15e10594a1c225794d9de45e51ed3126ba469
katzila <3qandre17@gmail.com>
2022-01-28 20:00:07 +0700

    комменты к index и app

--- src/App.js
+++ src/App.js
@@ -1,65 +1,68 @@
+//подключение react и деструтктуризация, достаём useState
 import React, { useState } from "react";
 
+//подключение используемых тут компонентов
 import List from "./components/List/List";
 import Content from "./components/content/Content";
 
 
-function App() {
-    const [tasks, setTasks] = useState([]);
-    const [currTask,setCurrTask]=useState({})
+function App() {// основной функциональный компонент приложения
+    const [tasks, setTasks] = useState([]);  //в состоянии основной функции хранится массив объектов - наши задачи
+    const [currTask,setCurrTask]=useState({}) // задача выбранная как isActive
 
+    //добавление новой задачи из формы newTask, на вход приходит уникальный id новой задачи и её заголовок
     const addNewTask = (newId, newLabel) => {
-        setTasks(tasks.map((task) => {
+        setTasks(tasks.map((task) => { //установка всех нынешних задач как не выбранных
             task.isActive = false;
         }))
         const newTask = {
-            id: newId,
-            label: newLabel,
-            text: "",
-            isActive: true,
-            status: 'waiting'
+            id: newId, //уникальный ид новой задачи
+            label: newLabel, //заголовок
+            text: "", //пока-что пустое содержание
+            isActive: true,// пометили только-что добавленную задачу как выбранную
+            status: 'waiting' // по-умолчанию статус задачи = ожидает
         };
-        setTasks([...tasks, newTask]);
-        setCurrTask({id:newId,label:newLabel,text:""})
+        setTasks([...tasks, newTask]); //добавляем новую задачу в массив
+        setCurrTask({id:newId,label:newLabel,text:""}) //устанавливаем её как текущую
     }
-    const changeTaskContent=(id,newlabel,newtext)=>{
+    const changeTaskContent=(id,newlabel,newtext)=>{ // обновление заголовка и содержания задачи пришедные из компонента content
         setTasks([
             ...tasks.map((task) => 
-                task.id === id ? { ...task, label: newlabel, text:newtext } : { ...task }
+                task.id === id ? { ...task, label: newlabel, text:newtext } : { ...task } //ищем нужную задачу по ид и обновляем её
             )
         ])
     }
-    const removeTask = (id) => {
+    const removeTask = (id) => { //удаление задачи по id пришедшему из content
         setTasks([
-            ...tasks.filter((task) => task.id !== id)
+            ...tasks.filter((task) => task.id !== id) //фильруем массив и возвращаем его-же, но без задачи с id
         ])
-        changeActiveTo(null);
-        setCurrTask(null);
+        changeActiveTo(null); //нет активных задач
+        setCurrTask(null); //нынешняя удалена
     }
-    const changeStatus = (id, newStatus) => {
+    const changeStatus = (id, newStatus) => { // меняем статус задачи с task.id==id на статус пришедный из компонента content
         setTasks([
             ...tasks.map((task) => 
-                task.id === id ? { ...task, status: newStatus } : { ...task }
+                task.id === id ? { ...task, status: newStatus } : { ...task }  //в копии массива ищем нужную задачу по id, если нашли - меняем статус, иначе возвращаем обратно как было
             )
         ])
     }
-    const changeActiveTo = (taskToChange) => {
-        if(taskToChange===null) return
+    const changeActiveTo = (taskToChange) => { //меняем активную задачу на пришедшую
+        if(taskToChange===null) return // если пришло ничего- ничего не делать
         setTasks([
             ...tasks.map((task) => 
-                task.id === taskToChange.id ? { ...task, isActive: true } : { ...task, isActive: false }
+                task.id === taskToChange.id ? { ...task, isActive: true } : { ...task, isActive: false }  // в копии массива ищем задачу с id как у нашей и активируем, остальные дизактивируем
             )
         ])
-        setCurrTask({id:taskToChange.id, label:taskToChange.label, text:taskToChange.text})
+        setCurrTask({id:taskToChange.id, label:taskToChange.label, text:taskToChange.text})  //делаем пришедную задачу текущей 
     }
-    return (
+    return (  //в основном div'e app - 2 div'а. leftbar- колонка с заголовками задач и кнопкой создания новой задачи, слева. content- основной блок для всех взаимодействий с текущей задачей
         <div className='app'>
-            <div className='app-leftbar'>
+            <div className='app-leftbar'> 
                 <List tasks={tasks} addNewTask={addNewTask} changeActiveTo={changeActiveTo} />
             </div>
-            <Content task={currTask} changeTaskContent={changeTaskContent} changeStatus={changeStatus} removeTask={removeTask} />
+            <Content task={currTask} changeTaskContent={changeTaskContent} changeStatus={changeStatus} removeTask={removeTask} /> 
         </div>
     )
 }
 
-export default App;
+export default App; //экпортируем компонент, для дальнейшого пользования
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+//подключение react
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//подключение корневого компонента
 import App from './App';
 
+//подключение стилей
 import './index.scss';
 import './components/NewTask/NewTask.scss'
 import './components/List/List.scss'
 import './components/content/Content.scss'
 import './components/Task/Task.scss'
 
-ReactDOM.render(
-    <React.StrictMode>
+
+//рендер всего приложения в Strict mode
+ReactDOM.render(      
+    <React.StrictMode>   
     <App />
     </React.StrictMode>,
     document.getElementById('root')
