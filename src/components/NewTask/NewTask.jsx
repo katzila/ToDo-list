925ac494fefd720538d0e5bc79d17ee29eb5e552
katzila <3qandre17@gmail.com>
2022-01-28 20:18:26 +0700

    комменты для формы и кнопки её вызывающей

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -1,20 +1,22 @@
+//+react
 import { useState } from "react";
 
+//компонент отвечающий за добавление новой задачи. Представляет форму с полем для написания заголовка, кнопкой добавления задачи, кнопкой закрытия формы без добавления задачи. А также кнопка для вызова этой формы
 function NewTask({ addNewTask }) {
-    const [isVisible, setIsVisible] = useState(false);
-    const [lastId, setLastId] = useState(0);
-    const [text, setText] = useState('');
+    const [isVisible, setIsVisible] = useState(false);//состояние формы, отображается ли она
+    const [lastId, setLastId] = useState(0); // последний использованный ид, lastID всегда будет больше id любой задачи
+    const [text, setText] = useState(''); // временное хранилище для поля ввода
 
-    const handleChange = (event) => {
+    const handleChange = (event) => { // если что-то ввели в поле - обновить text
         setText(event.target.value);
     }
-    const handleCliclOnNewTaskButton = () => {
+    const handleCliclOnNewTaskButton = () => { //при нажатии на кнопку "новая задача" - показать форму и увеличить ид, для новой задачи
         if (!isVisible) {
             setIsVisible(true);
             setLastId(lastId+1);
         }
     }
-    const handleCliclOnAddButton = () => {
+    const handleCliclOnAddButton = () => { //при нажатии кнопки добавить, если поле ввода не пусто- добавить задачу, иначе просто закрыть форму
         if (text==''){
             handleCliclOnExitButton();
             return;
@@ -23,19 +25,19 @@ function NewTask({ addNewTask }) {
         addNewTask(lastId, text);
         setText('');
     }
-    const handleCliclOnExitButton = () => {
+    const handleCliclOnExitButton = () => { //при нажатии на крестик закрыть форму и вернуться к старому lastId, текстовое поле очищается
         setIsVisible(false);
         setLastId(lastId-1);
         setText('');
     }
 
-    const handleEnterPress = (e) => {
+    const handleEnterPress = (e) => { //если нажать ввод добавляется новая задача, как при нажатии добавить
         if(e.key === "Enter"){
             handleCliclOnAddButton();
         }
     }
 
-    return (
+    return ( // рендер кнопки новая задача, при нажатии на которую рендерится форма, закрывается по нажатию на крестик, либо добавлении новой задачи
         <div className="newtask">
             <div onClick={handleCliclOnNewTaskButton} className="newtask-button">
 
